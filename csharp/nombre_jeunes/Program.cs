6787c2bbf648f818b68be23f25a731e9e6597e73
Coleric-FOAD <71262168+Coleric-FOAD@users.noreply.github.com>
2020-10-01 16:12:09 +0200

    csharp

---
+++ csharp/nombre_jeunes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace nombre_jeunes
+{
+    class Program
+    {
+        static int I;
+
+        static int N;
+
+        static string[] AGES;
+
+        static string saisie;
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez 20 âges : ");
+
+            saisie = Console.ReadLine();
+
+            Console.WriteLine("Vous avez écrit : " + saisie);
+
+            AGES = saisie.Split(' ');
+
+            for(I = 0; I < AGES.Length; I++)
+            {
+                    Console.WriteLine(AGES[I]);
+
+            }
+
+            Console.WriteLine("FINI");
+
+        }
+    }
+}

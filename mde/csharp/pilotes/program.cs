c924a813ca7c82a7a85601b5355e1cbfeeeb9be4
Coleric-FOAD <71262168+Coleric-FOAD@users.noreply.github.com>
2020-10-26 08:37:16 +0100

    26_10

---
+++ mde/csharp/pilotes/program.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace test_pilote
+{
+    class Program
+    {
+        static Pilote PiloteAvion;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Pilote");
+
+            PiloteAvion = new Pilote("Colonel", "Martin", "222", "date");
+            PiloteAvion = new Pilote("Lieutenant", "Dupont"), "333", "date2");
+
+            PiloteAvion.GetAge();
+            PiloteAvion.Piloter
+            PiloteAvion.Atterir
+            PiloteAvion.Decoller
+
+
+            Console.WriteLine(velo.GetGenre());
+            Console.WriteLine(moto.GetGenre());
+
+        }
+    }
+}
+

423e130521445e576795527c82472eb2f9345e7b
MDevoldere <mdevoldere@users.noreply.github.com>
2020-12-07 14:36:24 +0100

    debut winforms

---
+++ mde/winforms/Introduction/Calculatrice/FrmCalculatrice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculatrice
+{
+    public partial class FrmCalculatrice : Form
+    {
+        public FrmCalculatrice()
+        {
+            InitializeComponent();
+        }
+
+        public void Button_Click(object sender, EventArgs e)
+        {
+            Button bouton = sender as Button;
+
+            string texte = bouton.Text;
+
+            int chiffre = Convert.ToInt32(texte);
+
+            MessageBox.Show(texte);
+
+            txtResult.Text = texte;
+
+        }
+
+    }
+}

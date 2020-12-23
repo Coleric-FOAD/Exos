4bb6ce9e60b221083217e21671426f660a8fd0ac
MDevoldere <mdevoldere@users.noreply.github.com>
2020-12-23 15:47:42 +0100

    PS 2

---
+++ mde/powershell/intro/01_vars.ps1
@@ -0,0 +1,11 @@
+Get-Location
+
+$val = 2
+$val
+
+$saisie = Read-Host "Entrez un nombre positif: " 
+
+$saisie * $val
+
+"HAHA ! " * 3
+
---
+++ mde/powershell/intro/04_class.ps1
@@ -0,0 +1,16 @@
+
+Class Calcul 
+{
+	[Int] $nb1
+	[Int] $nb2
+
+	Calcul()
+	{
+		Write-Host "Constructeur de calcul"
+	}
+}
+
+$calcul = [Calcul]::new()
+
+$calcul2 = [Calcul]::new()
+
